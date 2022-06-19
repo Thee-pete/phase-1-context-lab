f0f083811c24eb4fac5ce748d77ea120c5d5cbb0
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-20 01:40:40 +0300

    commit tree f352ca2cbbb49eaa1b71359df9c68d8230e4a5b4

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(employee){
+    const employeeArray = {firstName: employee[0], familyName: employee[1],
+         title:employee[2], payPerHour:employee[3],
+          timeInEvents:[], timeOutEvents:[]}
+    return employeeArray
+}
+function createEmployeeRecords(records){
+    const employeeRecords = []
+    records.forEach(record => {
+        const employeeRecord = createEmployeeRecord(record)
+        employeeRecords.push(employeeRecord)
+    });
+    return employeeRecords;
+
+
+}
+function createTimeInEvent(dateStamp) {
+    const dateAndTime = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: dateAndTime[0],
+        hour: parseInt(dateAndTime[1]),
+    })
+    return this;
+}
+function createTimeOutEvent(dateStamp) {
+    const dateAndTime = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: dateAndTime[0],
+        hour: parseInt(dateAndTime[1]),
+    })
+    return this;
+}
+
+function hoursWorkedOnDate( dateStr) {
+    const timeIn = this.timeInEvents.find(record => record.date === dateStr)
+    const timeOut = this.timeOutEvents.find(record => record.date === dateStr)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate( dateStr) {
+    return hoursWorkedOnDate.call(this, dateStr) * this.payPerHour
+}
+
+
+
+function calculatePayroll(employeeRecordsArray){
+    let sum = 0
+    for (let i = 0; i < employeeRecordsArray.length; i++){
+      sum = sum + allWagesFor.call(employeeRecordsArray[i])
+    }
+    return sum 
+  }
+
+  function findEmployeeByFirstName(srcArray, firstName) {
+    let nameArray = []
+      srcArray.forEach(employee => {
+          if (employee.firstName === firstName) {
+            nameArray.push(employee);     
+            }
+        })
+  return nameArray[0];
+  } 
+
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
